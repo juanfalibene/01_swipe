9ecde89f0253776072736124cb6a0be6eb0231a2
Juan Ignacio Falibene <juanignaciofalibene@gmail.com>
2024-02-25 18:01:26 +0100

    Array de Objetos creado

---
+++ js/enchance_playlist_data.js
@@ -0,0 +1,36 @@
+const tracks = [
+  {
+    title: "All Need One Another",
+    artist: "Stylus",
+    previewUrl:
+      "https://p.scdn.co/mp3-preview/c9c416e6924591702a96eddabe35b92e9ac48478?cid=0788f3dec3064849bfa59c650a248b92",
+    imageUrl:
+      "https://i.scdn.co/image/ab67616d00001e0283e0779f2098c011890decef",
+  },
+  {
+    title: "Wild Fire",
+    artist: "Rebels",
+    previewUrl:
+      "https://p.scdn.co/mp3-preview/4695fe2bef2cef0cb54787aec200ed37a2830262?cid=0788f3dec3064849bfa59c650a248b9",
+    imageUrl:
+      "https://i.scdn.co/image/ab67616d0000b273a5116019a384ab131fc7dc92",
+  },
+  {
+    title: "Wired For Games",
+    artist: "C-Brand",
+    previewUrl:
+      "https://p.scdn.co/mp3-preview/c9c416e6924591702a96eddabe35b92e9ac48478?cid=0788f3dec3064849bfa59c650a248b92",
+    imageUrl:
+      "https://i.scdn.co/image/ab67616d0000b2737cb3f1fb879c7680f394491b",
+  },
+  {
+    title: "Stylus",
+    artist: "All Need One Another",
+    previewUrl:
+      "https://p.scdn.co/mp3-preview/a4bf63c1f5736beba053b6ec62738af8e2b29874?cid=0788f3dec3064849bfa59c650a248b92",
+    imageUrl:
+      "https://i.scdn.co/image/ab67616d0000b273824380ca79ff1e28277ff500",
+  },
+];
+
+export default tracks;
